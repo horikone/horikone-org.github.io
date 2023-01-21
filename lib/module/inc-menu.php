0ad384cad11a47008c2486023d59985ef314cb55
horikone <myjobs2000@mail.ru>
2023-01-21 17:14:46 +1000

    Initial commit

---
+++ lib/module/inc-menu.php
@@ -0,0 +1,39 @@
+<div class="wavebar-con-container-theme">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="menu-btn-backtotop">
+</div>
+<div class='menu-con-container'>
+	<div class="menu-con-backdrop darkmode-menu">
+	</div>
+	<div class="menu-con-outer">
+		<div class="menu-con-inner">
+			<div class="menu-con-logo">
+				<a href='/'>
+				<div class="menu-ico-logo">
+				</div>
+				</a>	
+			</div>
+			<a href='/articles'>
+			<div class="menu-btn-select">
+				<span>Статьи</span>
+			</div>
+			</a>
+			<a href='/faq'>
+			<div class="menu-btn-select">
+				<span>Отзывы</span>
+			</div>
+			</a>
+			<a href='/aboutus'>
+			<div class="menu-btn-select">
+				<span>О нас</span>
+			</div>
+		</div>
+	</div>
+</div>
